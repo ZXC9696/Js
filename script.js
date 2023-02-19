0122374874db3124fb455c8656b0bdea77a5c1ca
zxc <khaas0508@gmail.com>
2023-02-19 22:00:47 +0500

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+var name = prompt('Введите свое имя')
+var age = +prompt('Введите свой возраст')
+var sum = +prompt('6 + 3')
+var min = +prompt('10 - 7')
+var product = +prompt('3 x 2')
+var fra = +prompt('10 / 5')
+var rem = +prompt('5 / 2')
+var ani = alert('Перейдите в консоль')
+console.log(name);
+console.log(age);
+console.log('6 + 3 = 9 ' + '(Ваш ответ ' + sum + ')');
+console.log('10 - 7 = 3 ' + '(Ваш ответ ' + min + ')');
+console.log('3 x 2 = 6 ' + '(Ваш ответ ' + product + ')');
+console.log('10 / 5 = 2 ' + '(Ваш ответ ' + fra + ')');
+console.log('5 / 2 = 2.5 ' + '(Ваш ответ ' + rem + ')');
+
+var x = prompt(5)
+var y = prompt(6)
+var z = prompt(7)
+var res = prompt('5 + 6 + 7 ' + ' / 3')
+var all = alert((5 + 6 + 7) / 3)
